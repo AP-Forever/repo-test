51e6edde47faa5015a5c768b685e8a4db41f696c
AP-Forever <77897534+AP-Forever@users.noreply.github.com>
2026-06-12 00:56:44 -0400

    First commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+module.exports = {
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 3840], // or your custom values
+    // qualities: [75, 85, 90, 95]
+  },
 };
-
-export default nextConfig;
