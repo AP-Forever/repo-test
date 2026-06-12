7266898c36f911209b5a018a7804a1abb5f3b514
AP-Forever <77897534+AP-Forever@users.noreply.github.com>
2026-06-12 01:15:58 -0400

    server side props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,38 @@
 import Image from "next/image";
+import type { GetServerSideProps } from "next";
 
-export default function Page() {
+type Props = {};
+
+export default function Home({}: Props) {
   return (
-    <main>
-      <h1>Next.js Image Freeze Test</h1>
+    <main style={{ padding: "2rem" }}>
+      <h1>Next.js Image Sort Bug Test (Pages Router)</h1>
+      <p>Check Vercel logs for crash</p>
+
       <Image
-        src="https://picsum.photos/id/1015/800/600" // remote image
+        src="https://picsum.photos/id/1015/1200/800"
         alt="Test image"
-        width={800}
-        height={600}
-        quality={85}
+        width={1200}
+        height={800}
         priority
         sizes="(max-width: 768px) 100vw, 50vw"
+      />
+
+      {/* Multiple images to increase chance of hitting the code path */}
+      <Image
+        src="https://picsum.photos/id/237/800/600"
+        alt="Another test"
+        width={800}
+        height={600}
+        sizes="50vw"
       />
     </main>
   );
 }
+
+// Force SSR
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {},
+  };
+};
