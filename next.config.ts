d6e7dec889a0b39a2a4a62a6afae501b389ca78f
AP-Forever <77897534+AP-Forever@users.noreply.github.com>
2026-06-12 01:06:22 -0400

    test

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,11 @@
+const originalFreeze = Object.freeze;
+Object.freeze = function (obj: any) {
+  if (Array.isArray(obj) && obj.length > 0 && typeof obj[0] === "number") {
+    console.trace("=== FREEZING NUMBER ARRAY ===", obj);
+  }
+  return originalFreeze.call(this, obj);
+};
+
 module.exports = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 3840], // or your custom values
